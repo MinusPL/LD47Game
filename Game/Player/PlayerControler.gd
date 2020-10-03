31f6db51c2c6d5b0408fc0cfe370414bdec6e24c
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2020-10-03 19:24:51 +0200

    Added Teleports

--- Game/Player/PlayerControler.gd
+++ Game/Player/PlayerControler.gd
@@ -6,26 +6,40 @@ extends Area2D
 # var b = "text"
 
 export var m_iMoveSpeed = 200
-var m_vLastDirection = Vector2(1,0)
+var m_vLastDirection = Vector2(0,1)
 var m_sDirection = "down"
+var initialPosition = Vector2(0,0)
+var lock_movment = false
 
+func getInitialPosition():
+	return initialPosition
+
+func teleport(newPosition: Vector2, direction: String):
+	position = newPosition
+	m_sDirection = direction
+	lock_movment = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initialPosition = position
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2()
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+	if !lock_movment:
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= 1
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 1
+	else:
+		if !(Input.is_action_pressed("ui_right") ||  Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down")):
+			lock_movment = false
 
 	if velocity.length() > 0:
 		m_vLastDirection = velocity
