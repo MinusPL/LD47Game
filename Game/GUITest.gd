81893e5cb01a1252cf8cfa5751e7c64d38b3fe5a
Minus <theminusoid@gmail.com>
2020-10-05 19:31:46 +0200

    :V

--- Game/GUITest.gd
+++ Game/GUITest.gd
@@ -32,6 +32,7 @@ func _onInteraction(object):
 		$CanvasLayer/DialogueContainer/NameBackground/Label.text = object.getName()
 		$CanvasLayer/DialogueContainer/DialogueText.text = object.getDesc()
 		npc_interaction_state = InteractionState.DESCRIPTION
+		get_parent().setInventoryOpen(true)
 		current_interactable = object
 		player.setInteraction(true)
 		inventoryLocked = true
@@ -164,6 +165,7 @@ func process_interaction():
 			menu_option = 0
 	elif npc_interaction_state == InteractionState.INVENTORY:
 		var item_slots = $CanvasLayer/Inventory.getItemsSlots()
+		$CanvasLayer/Inventory.show()
 		$CanvasLayer/DialogueContainer/DialogueText.text = "Select item."
 		if len(item_slots) - $CanvasLayer/Inventory.getFreeSlotsCount():
 			item_slots[menu_option].setActiveFlag(true)
@@ -178,8 +180,10 @@ func process_interaction():
 			if Input.is_action_just_pressed("ui_accept"):
 				item_slots[menu_option].setActiveFlag(false)
 				npc_interaction_state = InteractionState.INVENTORY_ANSWER
+				$CanvasLayer/Inventory.hide()
 		if Input.is_action_just_pressed("ui_cancel"):
 			item_slots[menu_option].setActiveFlag(false)
+			$CanvasLayer/Inventory.hide()
 			npc_interaction_state = InteractionState.MAIN
 			menu_option = 0
 	elif npc_interaction_state == InteractionState.INVENTORY_ANSWER:
@@ -198,6 +202,7 @@ func process_interaction():
 		inventoryLocked = false
 		npc_interaction_state = InteractionState.NONE
 		player.setInteraction(false)
+		get_parent().setInventoryOpen(false)
 		lock_timestamp = OS.get_ticks_msec()
 		$CanvasLayer/DialogueContainer.hide()
 		$CanvasLayer/DialogueContainer/DialogueText.text = ""
--- Game/Scripts/Item/Items.gd
+++ Game/Scripts/Item/Items.gd
@@ -3,17 +3,35 @@ extends Node
 const itemImages = [
 	preload("res://Assets/Items/knife.png"),
 	preload("res://Assets/Items/vase.png"),
+	preload("res://Assets/Items/button.png"),
+	preload("res://Assets/Items/hair.png"),
+	preload("res://Assets/Items/paper.png"),
 ];
 
 const itemDictionary = {
 	0: {
-		"itemName": "Normal Knife",
+		"itemName": "Bloody knife",
 		"itemDesc": "Very wierd knife. Who cares about knives?",
 		"texture": itemImages[0],
 	},
 	1: {
-		"itemName": "Yyyy Waza",
+		"itemName": "Bloody vase",
 		"itemDesc": "Uuuuu URNA",
 		"texture": itemImages[1],
+	},
+	2: {
+		"itemName": "Gold button",
+		"itemDesc": "Uuuuu URNA",
+		"texture": itemImages[2],
+	},
+	3: {
+		"itemName": "Red hair",
+		"itemDesc": "Uuuuu URNA",
+		"texture": itemImages[3],
+	},
+	4: {
+		"itemName": "Piece of burnt paper",
+		"itemDesc": "Uuuuu URNA",
+		"texture": itemImages[4],
 	}
 };
